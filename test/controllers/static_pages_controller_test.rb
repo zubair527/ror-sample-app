846b7205caa2f2bcde72bb7cb2a995078d5ca24c
zubair muhammad <muhammad.zubair@tkxel.com>
2015-03-09 14:22:18 +0500

    updated dynamic title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,17 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Home | ROR Sample App'
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', 'Help | ROR Sample App'
   end
 
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select   'title', 'About | ROR Sample App'
   end
 
 end
