0533dda3058ece79cfba242e9916d3693a2ce0a1
zubair muhammad <muhammad.zubair@tkxel.com>
2015-03-10 14:57:17 +0500

    update custom.css.scss

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'sdoc',                 '0.4.0', group: :doc
 gem 'execjs'
 gem 'therubyracer'
 
+gem 'bootstrap-sass',       '3.2.0.0'
+
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
   gem 'byebug',      '3.4.0'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,4 +20,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select   'title', 'About | ROR Sample App'
   end
 
+  test "should get contact" do
+    get :about
+    assert_response :success
+    assert_select 'title', 'Contact | ROR Sample App'
+  end
+
 end
