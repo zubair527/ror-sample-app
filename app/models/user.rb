eda75a4c5f962e8c33d63cefdf1022456d5032b7
zubair muhammad <muhammad.zubair@tkxel.com>
2015-03-11 17:13:54 +0500

    comment fixture data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save {self.email.downcase!}
+
   validates(:name, presence: true, length: {maximum: 50 })
   validates(:email, presence:  true, length: {maximum: 255 }, uniqueness: {case_sensitive: false})
 end
