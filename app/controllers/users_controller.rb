353baeccfa5e3d16bf6b8fd8f1f35d82a747154d
zubair muhammad <muhammad.zubair@tkxel.com>
2015-03-13 13:06:44 +0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # do something interesting
+      flash[:success] = "Hi #{@user.name}, Welcome the zombie twitter :)"
       redirect_to @user
     else
       render 'new'
